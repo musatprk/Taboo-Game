19ad4a0b20e4dc6259095f8d5fb7f458f2a61444
musatprk <56595508+musatprk@users.noreply.github.com>
2021-02-07 14:53:44 +0300

    Update Form1.cs

--- tabu2/tabu2/Form1.cs
+++ tabu2/tabu2/Form1.cs
@@ -7,6 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+/*
+  
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX000KXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMWNK0kdoooodxOKNWMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMWXKOxooooooooolooxk0XWMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMWNX0kdoooloooooooooooooodxOKNWMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMWNKOxooolooooooooooooooooooolooxk0XWMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMWNX0kdollooloooooooooooooooooooooloooodk0KNWMMMMMMMMMMMMM
+MMMMMMMMMMWNKOxoollooooooloooooooooooooooooooooooooooooxOKNWMMMMMMMMMM
+MMMMMMMWX0kdoooooooooooooooooooooooooooooooooooooooooooooodk0XNMMMMMMM
+MMMMMMNkdoloooooooooooooooooooooooooooooooooooooooooooooooooooxOXWMMMM
+MMMMMMXxooooloooooooooooooooooooooooooooooooooooooooooooooolool:oKWMMM
+MMMMMMXdooooooooooooooooooooooooooooooooooooooooooooooooooolooc;:OWMMM
+MMMMMMXxooooooooooooooooooooooooooooooooooooooooooooooooooooooc;:OWMMM
+MMMMMMXxooooooooooooooooooooooooooooooooooooooooooooooooooooooc;:OWMMM
+MMMMMMXdooooooooooooooooooooooooooooooooooooooooooooooooooolooc;:OWMMM
+MMMMMMXdooooooooooooooooooooooooooooooooooooooooooooooooooolooc;:OWMMM
+MMMMMMXdooooooooooooooooooooooooooooooooooooooooooooooooooooooc;:OWMMM
+MMMMMMXdooooooooooooooooooooooooooooooooooooooooooooooooooooooc;:OWMMM 
+        _   _   _   _   _   _   _   _     _   _   _   _   _   _    
+       / \ / \ / \ / \ / \ / \ / \ / \   / \ / \ / \ / \ / \ / \ 
+      ( n | e | w | - | n | o | d | e ) ( s | o | c | i | a | l )
+       \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/
+MMMMMMXdooooooooooooooooooooooooooooooooooooooooooooooooooooooc;:OWMMM
+MMMMMMXdooooooooooooooooooooooooooooooooooooooooooooooooooooooc;:OWMMM
+MMMMMMXdooooooooooooooooooooooooooooooooooooooooooooooooooooooc;:OWMMM
+MMMMMMXdooooooooooooooooooooooooooooooooooooooooooooooooooooooc;:OWMMM
+MMMMMMXdooooooooooooooooooooooooooooooooooooooooooooooooooooooc;:OWMMM
+MMMMMMXxooooooooooooooooooooooooooooooooooooooooooooooooooooool;:OWMMM
+MMMMMMXxooolooooooooooooooooooooooooooooooooooooooooooooooooooc;:OWMMM
+MMMMMMW0xoloolooooooooooooooooooooooooooooooooooooooooooooollc;,c0WMMM
+MMMMMMMWXkdollloooooooooooooooooooooooooooooooooooooooollc:;,;:oONMMMM
+MMMMMMMMMWNKOxollloooooooooooooooooooooooooooooooooollc;,,;cdOXNWMMMMM
+MMMMMMMMMMMMMWX0kdolllooooolooooooooooooooooooooolc:;,,:ok0NWMMMMMMMMM
+MMMMMMMMMMMMMMMMWNKOxollloooloooooooooooooooollc:,,;cdOKNWMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMWX0kdollloooooooooooollc:;,,:lx0NWMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMWNKOxollloooooollc:;,;cdOKNWMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMWX0kdolllc:;,,:lx0XWMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNKOdoc:cdkKNWMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXK0KNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+*/
 
 namespace tabu2
 {
